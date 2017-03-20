0d67e5177c99cf7b9e374890b9cc810873fec1ff
Ben Mercer <ben@henroben.co.uk>
2017-03-20 16:16:18 +0000

    Redux-thunk added

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+
+
+export function signinUser( {email, password} ) {
+
+    return function(dispatch) {
+        // Submit email/password to server
+
+        // If request good
+        // - Update state to indicate user is authenticated
+        // - Save JWT token
+        // - redirect to the protected route
+
+        // If request bad
+        // - Show an error to user
+    }
+    
+}
